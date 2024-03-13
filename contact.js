6b2489821152e4ca05ea70769aa637b49c948c06
Deeksha Gawas <142161499+deekshag18@users.noreply.github.com>
2024-03-13 22:26:01 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,12 @@
+function check() {
+    var name=document.contact.uname;
+    var number = document.contact.uphone;
+    var mail=document.contact.umail;
+    var message = document.contact.message;
+    {
+        if(name=="")
+        {
+            alert("enter the name");
+        }
+    }
+}
